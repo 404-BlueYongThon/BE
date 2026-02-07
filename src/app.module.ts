bec9b68a157b4210105f9796ade07e31941c44eb
cau20232907 <hs20232907@cau.ac.kr>
2026-02-08 00:01:49 +0900

    refactor: prisma config 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 // import { PrismaService } from './prisma';
 import { EmergencySseService } from './emergency-sse.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 전역 사용 가능
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, EmergencySseService],
 })
