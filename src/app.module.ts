2a275ee4efac2f339e5484466c57662bfa55b427
cau20232907 <hs20232907@cau.ac.kr>
2026-02-08 11:36:55 +0900

    refactor: 의미 없는 파일 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { PrismaService } from './prisma';
 import { EmergencySseService } from './emergency-sse.service';
 import { ConfigModule } from '@nestjs/config';
 
